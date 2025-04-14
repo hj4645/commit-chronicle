9271057c9849367170914a784a4e90740756d7b1
hj4645 <hjpark4645@gmail.com>
2025-04-15 08:14:08 +0900

    feat: core 모듈 모델 코드 작성

---
+++ core/src/main/kotlin/model/Commit.kt
@@ -0,0 +1,34 @@
+package com.commitchronicle.core.model
+
+import java.time.ZonedDateTime
+
+/**
+ * Git 커밋 정보를 담는 데이터 클래스
+ */
+data class Commit(
+    val id: String,
+    val shortId: String,
+    val message: String,
+    val author: String,
+    val email: String,
+    val date: ZonedDateTime,
+    val changes: List<FileChange> = emptyList()
+)
+
+/**
+ * 파일 변경 정보를 담는 데이터 클래스
+ */
+data class FileChange(
+    val path: String,
+    val changeType: ChangeType,
+    val additions: Int,
+    val deletions: Int,
+    val diff: String
+)
+
+/**
+ * 변경 유형
+ */
+enum class ChangeType {
+    ADD, MODIFY, DELETE, RENAME, COPY
+} 
