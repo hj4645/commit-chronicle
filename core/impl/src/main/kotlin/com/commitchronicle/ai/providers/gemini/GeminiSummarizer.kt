8028a26d10debd04cd6334a11a06b455ef4690b9
hj4645 <hjpark4645@gmail.com>
2025-07-15 14:27:14 +0900

    fix: time out error

--- core/impl/src/main/kotlin/com/commitchronicle/ai/providers/gemini/GeminiSummarizer.kt
+++ core/impl/src/main/kotlin/com/commitchronicle/ai/providers/gemini/GeminiSummarizer.kt
@@ -7,6 +7,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
@@ -27,6 +28,11 @@ class GeminiSummarizer(config: AIProviderConfig) : BaseSummarizer(config) {
             json(Json {
                 ignoreUnknownKeys = true
             })
+        }
+        install(HttpTimeout) {
+            requestTimeoutMillis = 60_000
+            connectTimeoutMillis = 30_000
+            socketTimeoutMillis = 60_000
         }
     }
 
